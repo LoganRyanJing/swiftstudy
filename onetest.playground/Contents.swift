3038f55a4082d916512d8b4f75463184579afa1d
LoganMac <loganmac@bogon.dhcp>
2018-11-21 07:59:32 +0800

    swift可选型

---
+++ onetest.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//var str = "Hello, playground"
+var errorCode:Int? = 404
+errorCode = 0
+
+errorCode = nil
+var color:UIColor? = nil
+
+let imInt = 405
+errorCode = imInt
+print(errorCode)
+
+
+
